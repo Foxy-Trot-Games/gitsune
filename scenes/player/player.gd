7d7823d9b352bb5f1d15ef4ccd792e49e2f147a8
Crisp Lad <tcghyit@gmail.com>
2025-11-02 22:36:55 -0800

    added quick 'sound wave' emission from player using particles

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
+const PARTICLES = preload("uid://dmwmphp4y0bya")
+
 const HORIZONTAL_SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const KNOCKBACK_IMPULSE_Y: float = 300.0
@@ -11,14 +13,39 @@ func apply_knockback_impulse(direction: Vector2) -> void:
 	velocity += direction * KNOCKBACK_IMPULSE_Y
 	# clamp velocity so it doesn't keep going to infinity
 	velocity = velocity.clampf(-600,600)
+	
+	_create_sound_wave(direction)
+
+func _create_sound_wave(direction: Vector2) -> void:
+	# create new sound wave particle
+	var particles : GPUParticles2D = PARTICLES.instantiate()
+	particles.emitting = true
+	
+	# set particle emit direction
+	var process_material : ParticleProcessMaterial = particles.process_material
+	process_material.direction = Vector3(-direction.x, -direction.y, 0.0) 
+	
+	add_child(particles)
+	
+	# free up particle node after its lifetime is done
+	# todo probably a better way to do this
+	await particles.finished
+	particles.queue_free()
 
 # keyboard input
 func _unhandled_key_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_down"):
-		apply_knockback_impulse(Vector2.DOWN)
-		
+	
 	if event.is_action_pressed("ui_up"):
 		apply_knockback_impulse(Vector2.UP)
+	
+	# only allow up button to be pressed on the floor
+	# don't allow any others
+	if is_on_floor():
+		return
+	
+	if event.is_action_pressed("ui_down"):
+		apply_knockback_impulse(Vector2.DOWN)
+
 		
 	if event.is_action_pressed("ui_right"):
 		apply_knockback_impulse(Vector2.RIGHT)
