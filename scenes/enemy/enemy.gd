3e3e123aa3895203d1d84261e2e1ce33baf80b6c
yassine mathlouthi <yassinemt08@gmail.com>
2025-11-04 00:05:35 +0100

    fix unwanted warnings after reloading the scene

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-@export var PLAYER_NAME = "CharacterBody2D"
+@export var PLAYER_NAME:String = "CharacterBody2D"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -13,6 +13,11 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == PLAYER_NAME:
-		print(PLAYER_NAME+" entred the area of an enemy")
-		get_tree().reload_current_scene()
+		print(PLAYER_NAME + " entered the area of an enemy")
+		call_deferred("_restart_scene")
+
+
+func _restart_scene() -> void:
+	get_tree().reload_current_scene()
+	
 		
