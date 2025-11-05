a6f2149542d48be09076c5a394cec77d98f166fd
Jerrylikesgreen <jerrylikesgreen@gmail.com>
2025-11-04 22:38:30 -0500

    Add pulse action and improve player movement logic
    Introduces a new 'pulse' input action in project settings and implements its handling in pulse.gd, applying a knockback impulse and triggering a particle effect. Refactors player movement and sprite flipping logic in player.gd and player_controler.gd for better separation of concerns and responsiveness. Adjusts knockback impulse value and cleans up particle effect lifecycle management.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,7 @@
 # Player.gd
 class_name Player
 extends CharacterBody2D
+@onready var player_sprite: Sprite2D = %PlayerSprite
 
 @export var speed: int = 200
 @export var jump_force: int = 400
@@ -12,16 +13,20 @@ func _ready() -> void:
 
 func _on_player_movement_input_signal(direction: Vector2) -> void:
 	move_direction = direction
+	if direction == Vector2(1.0, 0.0) and player_sprite.flip_h:
+		player_sprite.set_flip_h(false)
+		
+	if direction == Vector2(-1.0, 0.0) and !player_sprite.flip_h:
+		player_sprite.set_flip_h(true)
+
 
 func _physics_process(delta: float) -> void:
 	# Horizontal movement
 	velocity.x = move_direction.x * speed
-
-	# Jumping
-
-
-	# Apply gravity
-	velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity") * delta
-
+	# Apply gravity 
+	if move_direction.y == 0.0:
+		velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity") * delta
+	else:
+		velocity.y = move_direction.y 
 	# Move player
 	move_and_slide()
