8934ad7e8f27962a51b66139376c90e2c6fe1881
yassine mathlouthi <yassinemt08@gmail.com>
2025-11-05 13:52:59 +0100

    adding a pathing enemy , flying enemy fix

---
+++ scenes/enemy/Flying_enemy/flying_enemy.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+@onready var vision: Area2D = $vision
+@onready var animated_sprite_2d: AnimatedSprite2D = $hitbox_area/AnimatedSprite2D
+@onready var vision_collision: CollisionShape2D = $vision/vision_collision
+@onready var hitbox_area: Area2D = $hitbox_area
+
+
+var seen : bool = false 
+var player : Player
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Random starting frame for animation
+	vision.body_entered.connect(_on_vision_body_entered)
+	vision.body_exited.connect(_on_vision_body_exited)
+	hitbox_area.body_entered.connect(_on_hitbox_area_body_entered)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if seen == true:
+		change_direction()
+
+
+func change_direction() -> void:
+	if player.position.x < position.x:
+		# Player is left of enemy
+		animated_sprite_2d.flip_h = false
+	else:
+		# Player is right of enemy
+		animated_sprite_2d.flip_h = true
+
+func _on_vision_body_entered(body: Node2D) -> void:
+	if body is Player:
+		seen=true
+		player=body
+
+func _on_vision_body_exited(body: Node2D) -> void:
+		if body is Player:
+			seen=false
+			player=null
+# Reload scene when player dies
+func _kill_player() -> void:
+	get_tree().reload_current_scene()
+
+func _on_hitbox_area_body_entered(body: Node2D) -> void:
+		if body is Player:
+			call_deferred("_kill_player")
