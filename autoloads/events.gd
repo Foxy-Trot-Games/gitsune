ad4dd8814eb44be222783f661ac6a0ba1fb67b61
Jerrylikesgreen <jerrylikesgreen@gmail.com>
2025-11-06 22:58:02 -0500

    Add HUD ammo display and improve ammo management
    Introduces a new HUD with an ammo display using a ProgressBar, managed by the new AmmoDisplay script. Refactors ammo pickup nodes into a single parent, updates signal and global variable handling for gun ammo, and ensures the HUD updates in response to gun equip and ammo changes. Also updates theme and project settings to support the new HUD.

--- autoloads/events.gd
+++ autoloads/events.gd
@@ -2,13 +2,29 @@ extends Node
 
 signal pulse_knockback_signal(direction: Vector2)
 signal player_movement_input_signal
-signal current_gun_ammo_signal(int)
+signal current_gun_ammo_signal(ammo_count: int)
+signal gun_equipped_signal(MAX_AMMO: int) ## TODO will need to send out either rresource or ref of Gun 
 
-func player_movement_input(direction:Vector2)->void:
+func player_movement_input(direction:Vector2) -> void:
 	emit_signal("player_movement_input_signal", direction)
 	
-func pulse_knockback(direction:Vector2)->void:
+func pulse_knockback(direction:Vector2) -> void:
 	emit_signal("pulse_knockback_signal", direction)
 
-func current_gun_ammo(ammo_count: int)->void:
+func current_gun_ammo(ammo_count: int) -> void:
+	var new_ammo_count: int = ammo_count + Globals.active_gun_ammo_count
+
+	if new_ammo_count > Globals.ACTIVE_GUN_MAX_AMMO:
+		var clamped_ammo_count: int = Globals.ACTIVE_GUN_MAX_AMMO - Globals.active_gun_ammo_count
+		
+		emit_signal("current_gun_ammo_signal", clamped_ammo_count)
+		print(Globals.ACTIVE_GUN_MAX_AMMO)
+		return
+
 	emit_signal("current_gun_ammo_signal", ammo_count)
+
+
+func gun_equipped(MAX_AMMO: int) -> void:
+	emit_signal("gun_equipped_signal", MAX_AMMO)
+	print(self, MAX_AMMO)
+	
