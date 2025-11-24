64626e263e86dbf0df8dfb0c46d5793325cb5ad5
Yassine-raiselabs <yassine@raiselabs.de>
2025-11-24 09:51:33 +0100

    enemy die

--- scenes/objects/scripts/spikes.gd
+++ scenes/objects/scripts/spikes.gd
@@ -8,3 +8,6 @@ func _ready() -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		Events.player_died.emit()
+	if body is IAEnemy:
+		body.queue_free()
+		print("yes enemy there")
