db02620ab3145cf9765bc2d85c2775e2ff03edb1
Crisp Lad <tcghyit@gmail.com>
2025-11-24 12:30:38 -0800

    added lazer hazard that can be added to levels

--- scenes/enemy/base_ia_enemy.gd
+++ scenes/enemy/base_ia_enemy.gd
@@ -5,4 +5,4 @@ class_name IAEnemy extends CharacterBody2D
 func _on_pulse_stun(knockback_direction: Vector2, radius: float, duration: float, knockback_force: float) -> void
 
 @abstract
-func die()
+func die() -> void
