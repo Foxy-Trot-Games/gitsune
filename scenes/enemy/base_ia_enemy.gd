962d702d3cfea95a12c01f7bf20c4006731ac492
Crisp Lad <tcghyit@gmail.com>
2025-11-24 11:38:16 -0800

    [code-only] added a HazardCheck node that makes it easier to check if player or enemies hit spikes or hazards, removed the spike scene in favor of using the tilemap version instead since it's faster

--- scenes/enemy/base_ia_enemy.gd
+++ scenes/enemy/base_ia_enemy.gd
@@ -3,3 +3,6 @@ class_name IAEnemy extends CharacterBody2D
 
 @abstract
 func _on_pulse_stun(knockback_direction: Vector2, radius: float, duration: float, knockback_force: float) -> void
+
+@abstract
+func die()
--- scripts/hazard_check.gd
+++ scripts/hazard_check.gd
@@ -1,7 +1,11 @@
 extends Node
 
-@onready var player := Globals.get_player()
 @onready var tile_map_layer: TileMapLayer = get_tree().get_first_node_in_group("InteractableTileMapLayer")
+
+var _parent : CharacterBody2D
+
+func _ready() -> void:
+	_parent = get_parent()
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta: float) -> void:
@@ -9,7 +13,7 @@ func _physics_process(delta: float) -> void:
 
 func check_tile_damage() -> void:
 	# convert global position to local map coordinates
-	var map_pos := tile_map_layer.local_to_map(player.global_position)
+	var map_pos := tile_map_layer.local_to_map(_parent.global_position)
 	
 	# get the TileData for the cell at that position
 	var tile_data := tile_map_layer.get_cell_tile_data(map_pos)
@@ -18,4 +22,7 @@ func check_tile_damage() -> void:
 	if tile_data:
 		var is_hazard : bool = tile_data.get_custom_data("is_hazard")
 		if is_hazard:
-			Events.player_died.emit()
+			if _parent is Player:
+				Events.player_died.emit()
+			elif _parent is IAEnemy:
+				(_parent as IAEnemy).die()
